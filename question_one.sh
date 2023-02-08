06b7e7497c5c3b9d5a03d97da889b5064327e4f1
rithish1126 <122535424+rithish1126@users.noreply.github.com>
2023-02-08 14:59:40 +0530

    Update question_one.sh

--- question_one.sh
+++ question_one.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Current Time is:$(date +"%T")"
-echo "Current date: $now"
+echo "Current date: $(DATE '+%d-%m-%y')"
 echo "USER IS:$USER"
 echo "HOME DIRECTORY IS:$HOME"
 echo "Current Working Directory Is:$PWD"
