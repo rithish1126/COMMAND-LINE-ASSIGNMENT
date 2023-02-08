cffc28d97b5d02a63169140132f331fb7b29dbdb
rithish1126 <122535424+rithish1126@users.noreply.github.com>
2023-02-08 14:33:16 +0530

    Update question_three.sh

--- question_three.sh
+++ question_three.sh
@@ -2,6 +2,10 @@
 echo "Enter the number: "  
 read n
 flag=0
+if [ $n -eq 1 ] || [ $n -eq 0 ]; then
+    echo "$n is Not Prime number"
+    return
+fi
 for (( i=2 ; i<$sqrt($n) ; i++ )); 
 do
     if [ $((n%i)) == 0 ];then
